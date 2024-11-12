1020213be940ccbca11c3e81f5255269684a86ab
David Kwon <david.kwon@clover.com>
2024-11-12 15:29:39 -0700

    Update syncSeed in seed.js to bulkCreate Menus and Items

--- seed.js
+++ seed.js
@@ -1,5 +1,7 @@
 const Restaurant = require("./models/index")
-const { seedRestaurant } = require("./seedData");
+const Menu = require("./models/Menu");
+const Item = require("./models/Item");
+const { seedRestaurant, seedMenu, seedItem } = require("./seedData");
 const db = require("./db/connection")
 
 const syncSeed = async () => {
@@ -7,7 +9,8 @@ const syncSeed = async () => {
     await Restaurant.bulkCreate(seedRestaurant)
     // BONUS: Update with Item and Menu bulkCreate
 
-
+    await Menu.bulkCreate(seedMenu);
+    await Item.bulkCreate(seedItem);
 }
 
 syncSeed()
