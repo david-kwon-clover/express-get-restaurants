69e1e523f09e033aaf831c961624c8a91308ea03
David Kwon <david.kwon@clover.com>
2024-11-12 15:26:31 -0700

    Add model associations in models/index.js

--- models/index.js
+++ models/index.js
@@ -2,6 +2,12 @@ const Restaurant = require('./Restaurant')
 const Menu = require("./Menu");
 const Item = require("./Item");
 
+Restaurant.hasMany(Menu);
+Menu.belongsTo(Restaurant);
+
+Item.belongsToMany(Menu);
+Menu.belongsToMany(Item);
+
 module.exports = { 
     Restaurant,
     Menu,
