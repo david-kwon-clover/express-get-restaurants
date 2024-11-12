469174b96f48842091dc37161720eb237d9a5b10
David Kwon <david.kwon@clover.com>
2024-11-12 11:44:49 -0700

    Add route for creating new restaurant (POST)

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 //TODO: Create your GET Request Route Below: 
 app.get("/restaurants", async (req, res, next) => {
     try {
@@ -23,6 +26,18 @@ app.get("/restaurants/:id", async (req, res, next) => {
             throw new Error("No restaurant found.");
         }
         res.json(restaurant);
+    } catch(error) {
+        next(error);
+    }
+})
+
+app.post("/restaurants", async (req, res, next) => {
+    try {
+        await Restaurant.create(req.body);
+        res.status(201);
+        res.json({
+            "Created": req.body
+        })
     } catch(error) {
         next(error);
     }
